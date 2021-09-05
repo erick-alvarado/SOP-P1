cca90d9b2072ffd9bbe170cbf239a4c163a17873
SevenPok <57465474+SevenPok@users.noreply.github.com>
2021-09-04 19:51:13 -0600

    Rutas preparadas

--- api/go/src/main.go
+++ api/go/src/main.go
@@ -25,17 +25,33 @@ var tasks = allTasks{
 	},
 }
 
+func iniciarCarga(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func publicar(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func finalizarCarga(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func indexRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to my api")
 }
 
-func getTasks(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(tasks)
-}
-
 func main() {
+	//Creamos el router
 	router := mux.NewRouter().StrictSlash(true)
+	//Ruta principal
 	router.HandleFunc("/", indexRoute)
-	router.HandleFunc("/tasks", getTasks)
+	//Ruta para conectarse a la base de datos y esperar los datos
+	router.HandleFunc("/iniciarCarga", iniciarCarga)
+	//Ruta para publicar la informacion de la base de datos
+	router.HandleFunc("/publicar", publicar)
+	//Ruta para cerrar la conexion de la base de datos y mandar una notificacion
+	router.HandleFunc("/finalizarCarga", publicar)
+	//Escuchamos al puerto
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
