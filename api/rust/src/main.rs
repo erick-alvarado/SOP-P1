65c42bf35c9d6b1838494a0c0630a627eef85e64
Osman Pérez <2999565980101@ingenieria.usac.edu.gt>
2021-09-20 01:17:37 -0600

    Api Rust

--- api/rust/src/main.rs
+++ api/rust/src/main.rs
@@ -2,6 +2,8 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate gcp_pubsub;
+extern crate goauth;
 
 use rocket_contrib::json::Json;
 use std::env;
@@ -11,6 +13,7 @@ use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
 use std::time::{Duration, SystemTime};
 use std::thread::sleep;
+use mongodb::{bson::doc, sync::Client};
 
 #[derive(Clone, Deserialize, Serialize, Debug)]
 struct Publicacion {
@@ -36,9 +39,17 @@ static mut Pub_mysql : PubSub = PubSub{
   tiempoDeCarga: 0,
   bd : String::new(),
 };
+static mut Pub_mongo : PubSub = PubSub{
+  guardados : 0,
+  api : String::new(),
+  tiempoDeCarga: 0,
+  bd : String::new(),
+};
 
 
-/*unsafe fn lol(){
+
+
+unsafe fn lol(){
   let sys_time = SystemTime::now();
   let one_sec = Duration::from_secs(20);
   sleep(one_sec);
@@ -47,29 +58,79 @@ static mut Pub_mysql : PubSub = PubSub{
   .expect("Clock may have gone backwards");
   Pub_mysql.tiempoDeCarga = difference.as_secs();
   println!("{} {} {} {}", Pub_mysql.tiempoDeCarga, Pub_mysql.guardados, Pub_mysql.api, Pub_mysql.bd);
-}*/
+}
 
-fn main(){
-  dotenv().expect(".env file not found");
 
-  #[get("/")]
+#[get("/")]
   fn index() -> &'static str {
       "Bienvenido API Rust"
   }
 
   #[get("/iniciarCarga")]
   fn iniciar_carga() -> &'static str {
+
+    let mut pm : PubSub = PubSub{
+      guardados : 0,
+      api : String::from("rust"),
+      tiempoDeCarga: 0,
+      bd : String::from("Google SQL"),
+    };
+    let mut pc : PubSub = PubSub{
+      guardados : 0,
+      api : String::from("rust"),
+      tiempoDeCarga: 0,
+      bd : String::from("Cosmo DB"),
+    };
+    unsafe{
+      Pub_mysql = pm;
+      Pub_mongo = pc;
+    }
     "Carga Iniciada"
   }
 
   #[get("/finalizarCarga")]
   fn finalizar_carga() -> &'static str {
+    unsafe{
+      println!("{},{},{},{}",Pub_mysql.api,Pub_mysql.bd,Pub_mysql.tiempoDeCarga,Pub_mysql.guardados);
+    }
+    unsafe{
+      println!("{},{},{},{}",Pub_mongo.api,Pub_mongo.bd,Pub_mongo.tiempoDeCarga,Pub_mongo.guardados);
+    }
+    
+    unsafe{
+      let google_credentials = env::var("GOOGLE_APPLICATION_CREDENTIALS").expect("failed to get MONGODB_URL");
+      let topic_name = env::var("TOPIC").expect("failed to get MONGODB_URL");
+      let credentials = goauth::credentials::Credentials::from_file(&google_credentials).unwrap();
+      let mut client = gcp_pubsub::Client::new(credentials);
+      let topic = client.topic(&topic_name);
+      let serialized_user = serde_json::to_string(&Pub_mysql).unwrap();
+      let result = topic.publish(serialized_user);
+      let serialized_user2 = serde_json::to_string(&Pub_mongo).unwrap();
+      let result2 = topic.publish(serialized_user2);
+    }
+
+    let mut pm : PubSub = PubSub{
+      guardados : 0,
+      api : String::from("rust"),
+      tiempoDeCarga: 0,
+      bd : String::from("Google SQL"),
+    };
+    let mut pc : PubSub = PubSub{
+      guardados : 0,
+      api : String::from("rust"),
+      tiempoDeCarga: 0,
+      bd : String::from("Cosmo DB"),
+    };
+    unsafe{
+      Pub_mysql = pm;
+      Pub_mongo = pc;
+    }
     "Carga Finalizada"
   }
 
   #[post("/publicar", format="json", data = "<json>")]
   fn publicar(json: Json<Publicacion>)  {
-
+    let sys_time = SystemTime::now();
     let mysql_host = env::var("HOST_MYSQL").unwrap();
     let mysql_user = env::var("USER_MYSQL").unwrap();
     let mysql_pass = env::var("PASS_MYSQL").unwrap();
@@ -82,6 +143,7 @@ fn main(){
     let mut conn = pool.get_conn().unwrap();
 
     let user = json.into_inner();
+    let us = user.clone();
 
     let split: Vec<_> = user.fecha.as_str().split("/").collect();
     let mut fecha_nueva = String::new();
@@ -108,7 +170,42 @@ fn main(){
           "_downvotes" => user.downvotes,
       },
     ).unwrap();
+
+    unsafe{
+      let new_sys_time = SystemTime::now();
+      let difference = new_sys_time.duration_since(sys_time)
+      .expect("Clock may have gone backwards");
+      println!("{}", difference.as_secs());
+      Pub_mysql.tiempoDeCarga = Pub_mysql.tiempoDeCarga + difference.as_secs();
+      Pub_mysql.guardados = Pub_mysql.guardados +1;
+    }
+
+    let sys_time2 = SystemTime::now();
+    let conn_string = std::env::var_os("MONGODB_URL").expect("missing environment variable MONGODB_URL").to_str().expect("failed to get MONGODB_URL").to_owned();
+    
+    let todos_db_name = std::env::var_os("MONGODB_DATABASE").expect("missing environment variable MONGODB_DATABASE").to_str().expect("failed to get MONGODB_DATABASE").to_owned();
+
+    let todos_collection_name = std::env::var_os("MONGODB_COLLECTION").expect("missing environment variable MONGODB_COLLECTION").to_str().expect("failed to get MONGODB_COLLECTION").to_owned();
+
+    let mongo_client = Client::with_uri_str(&*conn_string).expect("failed to create client");
+    let todo_coll = mongo_client.database(todos_db_name.as_str()).collection(todos_collection_name.as_str());
+    let todo_doc = mongodb::bson::to_bson(&us).expect("struct to BSON conversion failed").as_document().expect("BSON to Document conversion failed").to_owned();
+        
+    let r = todo_coll.insert_one(todo_doc, None).expect("failed to add todo");
+
+    unsafe{
+      let new_sys_time2 = SystemTime::now();
+      let difference2 = new_sys_time2.duration_since(sys_time2)
+      .expect("Clock may have gone backwards");
+      println!("{}", difference2.as_secs());
+      Pub_mongo.tiempoDeCarga = Pub_mongo.tiempoDeCarga + difference2.as_secs();
+      Pub_mongo.guardados = Pub_mongo.guardados +1;
+    }
   }
 
+fn main(){
+  dotenv().expect(".env file not found");
+  
+  
   rocket::ignite().mount("/endpoint/rust",routes![index, iniciar_carga, finalizar_carga, publicar],).launch();   
 }
