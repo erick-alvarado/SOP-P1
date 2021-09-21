2b0b2b126760b309358b9293b585258e9a3044d6
Osman Pérez <2999565980101@ingenieria.usac.edu.gt>
2021-09-20 20:54:23 -0600

    load tester
    load tester de python terminada

--- Load Tester/python/main.py
+++ Load Tester/python/main.py
@@ -5,8 +5,5 @@ print("Leer archivos")
 leer = json.loads(open('data.json').read())
 
 for publicacion in leer:
-    respuesta = requests.post("localhost:8000/endpoint/rust/publicar", json=publicacion)
-    como_json = respuesta.json()
-    print("respuesta: ")
-    print(como_json)
+    respuesta = requests.post("http://localhost:8000/endpoint/rust/publicar", json=publicacion)
     
