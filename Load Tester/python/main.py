fee41b9990f1ac27642ff2acb68ccb117e2f5811
Osman Pérez <2999565980101@ingenieria.usac.edu.gt>
2021-09-21 15:56:45 -0600

    Update main.py

--- Load Tester/python/main.py
+++ Load Tester/python/main.py
@@ -3,15 +3,15 @@ import requests
 
 print("Leer archivos")
 leer = json.loads(open('data.json').read())
-respuesta_iniciar_rust = requests.get("http://34.120.185.238:80/endpoint/rust/iniciarCarga")
-respuesta_iniciar_go = requests.get("http://34.120.185.238:80/endpoint/go/iniciarCarga")
-respuesta_iniciar_python = requests.get("http://34.120.185.238:80/python/iniciarCarga")
+respuesta_iniciar_rust = requests.get("")
+respuesta_iniciar_go = requests.get("")
+respuesta_iniciar_python = requests.get("")
 
 for publicacion in leer:
-    respuesta_publicar_rust = requests.post("http://34.120.185.238:80/endpoint/rust/publicar", json=publicacion)
-    respuesta_publicar_go = requests.post("http://34.120.185.238:80/endpoint/go/publicar", json=publicacion)
-    respuesta_publicar_python = requests.post("http://34.120.185.238:80/python/publicar", json=publicacion)
+    respuesta_publicar_rust = requests.post("", json=publicacion)
+    respuesta_publicar_go = requests.post("", json=publicacion)
+    respuesta_publicar_python = requests.post("", json=publicacion)
 
-respuesta_finalizar_rust = requests.get("http://34.120.185.238:80/endpoint/rust/finalizarCarga")
-respuesta_finalizar_go = requests.get("http://34.120.185.238:80/endpoint/go/finalizarCarga")
-respuesta_finalizar_python = requests.get("http://34.120.185.238:80/python/finalizarCarga")
+respuesta_finalizar_rust = requests.get("")
+respuesta_finalizar_go = requests.get("")
+respuesta_finalizar_python = requests.get("")
