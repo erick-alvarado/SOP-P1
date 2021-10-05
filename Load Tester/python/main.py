e259d872acad4e28f42ee736814e499d992904da
Osman Pérez <2999565980101@ingenieria.usac.edu.gt>
2021-10-04 19:02:54 -0600

    kernel

--- Load Tester/python/main.py
+++ Load Tester/python/main.py
@@ -4,5 +4,5 @@ import requests
 print("Leer archivos")
 leer = json.loads(open('data.json').read())
 for publicacion in leer:
-    respuesta_publicar_rust = requests.post('http://34.120.185.238/rust/publicar', json=publicacion)
+    respuesta_publicar_rust = requests.post('http://34.120.185.238:80/publicar', json=publicacion)
     print(respuesta_publicar_rust.ok)
